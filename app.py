b848f536dc0986063308361edbf36073d9584cb7
shri50 <shridharshende50@gmail.com>
2022-11-01 20:54:41 +0530

    render template changed

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('new_design.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods= ['POST'])
 def predict():
